44299b26d7d6dff2f060cca0732e511072468249
Jmar33 <josimarferreira922@yahoo.com.br>
2025-07-29 08:43:10 -0300

    feat: Implementing exceptions

---
+++ rest-with-spring-boot-and-java-erudito/src/main/java/br/com/jmar33/math/SimpleMath.java
@@ -0,0 +1,32 @@
+package br.com.jmar33.math;
+
+import br.com.jmar33.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public Double subtraction(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public Double multiplication(Double numberOne, Double numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    public Double division( Double numberOne, Double numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    public Double mean(Double numberOne, Double numberTwo) {
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public Double squareRoot(Double number) {
+        return  Math.sqrt(number);
+    }
+}
